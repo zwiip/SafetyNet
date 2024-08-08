3d22a99000c21d318278b4f81c511e44c56fcbf3
abrugeilles <abrugeilles@ardans.fr>
2024-08-08 11:29:05 +0200

    creating FireStation Model

---
+++ src/main/java/com/safetynet/alerts/model/FireStation.java
@@ -0,0 +1,39 @@
+package com.safetynet.alerts.model;
+
+public class FireStation {
+    private String address;
+    private String station;
+
+    public FireStation() {}
+
+    public FireStation(String address, String station) {
+        this.address = address;
+        this.station = station;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+
+    @Override
+
+    @Override
+    public String toString() {
+        return "FireStation{" +
+                "address='" + address +
+                ", station='" + station +
+                '}';
+    }
+}
