5753bc22c043c3c3d390524f758ce16be45311b5
abrugeilles <abrugeilles@ardans.fr>
2024-08-21 12:11:10 +0200

    Fin des tests pour PersonRepository + test pour MedicalRecordRepository

---
+++ src/test/java/com/safetynet/alerts/repository/MedicalRecordRepositoryTest.java
@@ -0,0 +1,91 @@
+package com.safetynet.alerts.repository;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.safetynet.alerts.model.MedicalRecord;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MedicalRecordRepositoryTest {
+    private MedicalRecordRepository repository;
+
+    @BeforeEach
+    public void setUp() {
+        repository = new MedicalRecordRepository();
+    }
+
+    @Test
+    void createListMedicalRecords_shouldPopulateMedicalRecordsList() throws IOException {
+        // Arrange
+        String jsonContent = "{ \"medicalrecords\": [" +
+                "{ \"firstName\":\"John\", \"lastName\":\"Boyd\", \"birthdate\":\"03/06/1984\", \"medications\":[\"aznol:350mg\", \"hydrapermazol:100mg\"], \"allergies\":[\"nillacilan\"] }," +
+                "{ \"firstName\":\"Jacob\", \"lastName\":\"Boyd\", \"birthdate\":\"03/06/1989\", \"medications\":[\"pharmacol:5000mg\", \"terazine:10mg\", \"noznazol:250mg\"], \"allergies\":[] }" +
+                "] }";
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonContent);
+
+        // Act
+        repository.createListMedicalRecords(jsonNode);
+
+        // Assert
+        List<MedicalRecord> medicalRecords = repository.findAll();
+        assertEquals(2, medicalRecords.size());
+        assertEquals("John", medicalRecords.get(0).getFirstName());
+        assertEquals("Jacob", medicalRecords.get(1).getFirstName());
+    }
+
+    @Test
+    void findAll_shouldReturnAllMedicalRecords() {
+        // Arrange
+        List<MedicalRecord> expectedMedicalRecords = new ArrayList<>();
+        expectedMedicalRecords.add(new MedicalRecord("Roger", "Boyd", "09/06/2017", new ArrayList<>(List.of("")), new ArrayList<>(List.of(""))));
+        expectedMedicalRecords.add(new MedicalRecord("Felicia", "Boyd", "01/08/1986", new ArrayList<>(List.of("tetracyclaz:650mg")),new ArrayList<>(List.of("xilliathal"))));
+
+        repository.medicalRecords = expectedMedicalRecords;
+
+        // Act
+        List<MedicalRecord> actualMedicalRecords = repository.findAll();
+
+        // Assert
+        assertEquals(expectedMedicalRecords, actualMedicalRecords);
+    }
+
+    @Test
+    void findMedicalRecordByFullName_shouldReturnCorrectMedicalRecord() {
+        // Arrange
+        List<MedicalRecord> medicalRecordList = new ArrayList<>();
+        medicalRecordList.add(new MedicalRecord("Roger", "Boyd", "09/06/2017", new ArrayList<>(List.of("")), new ArrayList<>(List.of(""))));
+        medicalRecordList.add(new MedicalRecord("Felicia", "Boyd", "01/08/1986", new ArrayList<>(List.of("tetracyclaz:650mg")),new ArrayList<>(List.of("xilliathal"))));
+
+        repository.medicalRecords = medicalRecordList;
+
+        // Act
+        MedicalRecord wantedMedicalRecord = repository.findMedicalRecordsByFullName("Felicia", "Boyd");
+
+        // Assert
+        assertNotNull(wantedMedicalRecord);
+        assertEquals("Felicia", wantedMedicalRecord.getFirstName());
+        assertEquals("Boyd", wantedMedicalRecord.getLastName());
+    }
+
+    @Test
+    void findMedicalRecordByFullName_shouldReturnNull_whenNoMatchFound() {
+        // Arrange
+        List<MedicalRecord> medicalRecordList = new ArrayList<>();
+        medicalRecordList.add(new MedicalRecord("Roger", "Boyd", "09/06/2017", new ArrayList<>(List.of("")), new ArrayList<>(List.of(""))));
+        medicalRecordList.add(new MedicalRecord("Felicia", "Boyd", "01/08/1986", new ArrayList<>(List.of("tetracyclaz:650mg")),new ArrayList<>(List.of("xilliathal"))));
+
+        repository.medicalRecords = medicalRecordList;
+
+        // Act
+        MedicalRecord wantedMedicalRecord = repository.findMedicalRecordsByFullName("Anne", "Shirley");
+
+        // Assert
+        assertNull(wantedMedicalRecord);
+    }
+}
--- src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
+++ src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
@@ -1,9 +1,12 @@
 package com.safetynet.alerts.repository;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.safetynet.alerts.model.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +19,25 @@ public class PersonRepositoryTest {
     @BeforeEach
     public void setUp() {
         repository = new PersonRepository();
+    }
+
+    @Test
+    void createListPersons_shouldPopulatePersonList() throws IOException {
+        // Arrange
+        String jsonContent = "{ \"persons\": [" +
+                "{\"firstName\": \"Anne\", \"lastName\": \"Shirley\", \"address\": \"Green Gables\", \"city\": \"Avonlea\", \"zip\": \"12345\", \"phone\": \"0123456789\", \"email\": \"anne.shirley@avonlea.com\"}," +
+                "{\"firstName\": \"Diana\", \"lastName\": \"Barry\", \"address\": \"Orchard Slope\", \"city\": \"Avonlea\", \"zip\": \"12345\", \"phone\": \"0987654321\", \"email\": \"diana.barry@avonlea.com\"}" +
+                "] }";
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonContent);
+
+        // Act
+        repository.createListPersons(jsonNode);
+
+        // Assert
+        List<Person> persons = repository.findAll();
+        assertEquals(2, persons.size());
+        assertEquals("Anne", persons.get(0).getFirstName());
+        assertEquals("Diana", persons.get(1).getFirstName());
     }
 
     @Test
@@ -67,4 +89,73 @@ public class PersonRepositoryTest {
         // Assert
         assertNull(wantedPerson);
     }
+
+    @Test
+    void findPersonByLastName_shouldReturnAListOfCorrectPersons() {
+        // Arrange
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        persons.add(new Person("Diana", "Barry","Orchard Slope", "Avonlea", "12345", "0987654321", "diana.barry@avonlea.com" ));
+        persons.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        persons.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        List<Person> expectedPersons = new ArrayList<>();
+        expectedPersons.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        expectedPersons.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        repository.persons = persons;
+
+        // Act
+        List<Person> wantedPersonsList = repository.findPersonsByLastName("Cuthbert");
+
+        // Assert
+        assertNotNull(wantedPersonsList);
+        assertEquals(expectedPersons.toString(), wantedPersonsList.toString());
+    }
+
+    @Test
+    void findPersonsByAddress_shouldReturnAListOfCorrectPersons() {
+        // Arrange
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        persons.add(new Person("Diana", "Barry","Orchard Slope", "Avonlea", "12345", "0987654321", "diana.barry@avonlea.com" ));
+        persons.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        persons.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        repository.persons = persons;
+
+        List<Person> expectedPersons = new ArrayList<>();
+        expectedPersons.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        expectedPersons.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        expectedPersons.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        // Act
+        List<Person> wantedPersonsList = repository.findPersonByAddress("Green Gables");
+
+        // Assert
+        assertNotNull(wantedPersonsList);
+        assertEquals(expectedPersons.toString(), wantedPersonsList.toString());
+    }
+
+    @Test
+    void findPersonsByCity_shouldReturnAListOfCorrectPersons() {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        persons.add(new Person("Diana", "Barry","Orchard Slope", "Avonlea", "12345", "0987654321", "diana.barry@avonlea.com" ));
+        persons.add(new Person("Josephine", "Barry","Big House", "Charlottetown", "54321", "135798642", "josephine.barry@aunt.com" ));
+
+        repository.persons = persons;
+
+        List<Person> expectedPersons = new ArrayList<>();
+        expectedPersons.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        expectedPersons.add(new Person("Diana", "Barry","Orchard Slope", "Avonlea", "12345", "0987654321", "diana.barry@avonlea.com" ));
+
+        // Act
+        List<Person> wantedPersonsList = repository.findPersonsByCity("Avonlea");
+
+        // Assert
+        assertNotNull(wantedPersonsList);
+        assertEquals(expectedPersons.toString(), wantedPersonsList.toString());
+    }
+
 }
