0daf8beb54a33b9f01c448f64ad2050b96061359
abrugeilles <abrugeilles@ardans.fr>
2024-08-22 17:09:08 +0200

    wip : writing tests for methods in PersonService Class

--- src/test/java/com/safetynet/alerts/service/PersonServiceTest.java
+++ src/test/java/com/safetynet/alerts/service/PersonServiceTest.java
@@ -1,5 +1,9 @@
 package com.safetynet.alerts.service;
 
+import com.safetynet.alerts.controller.dto.ChildAlertDTO;
+import com.safetynet.alerts.controller.dto.MedicalRecordDTO;
+import com.safetynet.alerts.controller.dto.PersonInfoLastNameDTO;
+import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.model.Person;
 import com.safetynet.alerts.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
@@ -21,6 +25,10 @@ class PersonServiceTest {
 
     @MockBean
     private PersonRepository personRepositoryMock;
+
+    @MockBean
+    private MedicalRecordService medicalRecordService;
+
 
     @Test
     void getPersons_shouldReturnListOfAllPersonsFromRepository() {
@@ -52,6 +60,65 @@ class PersonServiceTest {
         // Assert
         assertEquals(expectedPerson, wantedPerson);
         verify(personRepositoryMock).findPersonByFullName("Anne", "Shirley");
+    }
+
+    @Test
+    void getPersonsByLastName_shouldReturnALisOfCorrectPersons() {
+        // Arrange
+        List<Person> expectedPersons = new ArrayList<>();
+        expectedPersons.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        expectedPersons.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        doReturn(expectedPersons).when(personRepositoryMock).findPersonsByLastName("Cuthbert");
+        doReturn(new MedicalRecord("Marrila", "Cuthbert", "01/01/1960", new ArrayList<>(List.of("eyedrops:2drops")), new ArrayList<>(List.of("")))).when(medicalRecordService).getOneMedicalRecord("Marrila", "Cuthbert");
+        doReturn(new MedicalRecord("Matthew", "Cuthbert", "01/01/1955", new ArrayList<>(List.of("heartpills:100mg")), new ArrayList<>(List.of("")))).when(medicalRecordService).getOneMedicalRecord("Matthew", "Cuthbert");
+
+        // Act
+        List<PersonInfoLastNameDTO> wantedPersons = personService.getPersonsByLastName("Cuthbert");
+
+        // Assert
+        assertEquals("marrila.cuthbert@avonlea.com", wantedPersons.get(0).getMail());
+        assertEquals("matthew.cuthbert@avonlea.com", wantedPersons.get(1).getMail());
+        verify(personRepositoryMock).findPersonsByLastName("Cuthbert");
+    }
+
+    @Test
+    void getPersonsByAddress_shouldReturnAListOfCorrectPersons() {
+        // Arrange
+        List<Person> expectedPersons = new ArrayList<>();
+        expectedPersons.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        expectedPersons.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        expectedPersons.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        doReturn(expectedPersons).when(personRepositoryMock).findPersonByAddress("Green Gables");
+
+        // Act
+        List<Person> wantedPersons = personService.getPersonsByAddress("Green Gables");
+
+        // Assert
+        assertEquals(expectedPersons, wantedPersons);
+        verify(personRepositoryMock).findPersonByAddress("Green Gables");
+    }
+
+    @Test
+    void createChildAlertList_shouldReturnAChildAlertDTO() {
+        // Arrange
+        List<Person> personsAtThisAddress = new ArrayList<>();
+        personsAtThisAddress.add(new Person("Anne", "Shirley", "Green Gables", "Avonlea", "12345", "0123456789", "anne.shirley@avonlea.com" ));
+        personsAtThisAddress.add(new Person("Marrila", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "marrila.cuthbert@avonlea.com" ));
+        personsAtThisAddress.add(new Person("Matthew", "Cuthbert", "Green Gables", "Avonlea", "12345", "0123456789", "matthew.cuthbert@avonlea.com" ));
+
+        doReturn(personsAtThisAddress).when(personRepositoryMock).findPersonByAddress("Green Gables");
+        doReturn(Long.valueOf(55)).when(medicalRecordService).getAge("Matthew", "Cuthbert");
+        doReturn(Long.valueOf(45)).when(medicalRecordService).getAge("Marrila", "Cuthbert");
+        doReturn(Long.valueOf(13)).when(medicalRecordService).getAge("Anne", "Shirley");
+
+        // Act
+        ChildAlertDTO actualChildAlert = personService.createChildAlertList("Green Gables");
+
+        // Assert
+        assertEquals(1, actualChildAlert.getChildList().size());
+        assertEquals(2, actualChildAlert.getOtherMembersList().size());
     }
 
 }
