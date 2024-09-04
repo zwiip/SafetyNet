7961e1a3e8e8177cfb5f64c9e1f2bb8485a0aa73
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-09-04 16:00:51 +0200

    correction des tests pour PersonRepository

--- src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
+++ src/test/java/com/safetynet/alerts/repository/PersonRepositoryTest.java
@@ -67,7 +67,6 @@ public class PersonRepositoryTest {
         assertNull(wantedPerson);
     }
 
-    // TODO j'en suis ici
     @Test
     void findPersonByLastName_shouldReturnAListOfCorrectPersons() {
         // Act
