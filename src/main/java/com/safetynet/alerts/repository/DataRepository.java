32b274e0e2f96af53b5a2d366534a057e7911280
abrugeilles <abrugeilles@ardans.fr>
2024-09-10 15:46:54 +0200

    wip : writing comments and loggers

--- src/main/java/com/safetynet/alerts/repository/DataRepository.java
+++ src/main/java/com/safetynet/alerts/repository/DataRepository.java
@@ -2,6 +2,8 @@ package com.safetynet.alerts.repository;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.io.File;
@@ -9,9 +11,13 @@ import java.io.IOException;
 
 @Repository
 public class DataRepository {
+
+    /** VARIABLES **/
+    Logger logger = LoggerFactory.getLogger(DataRepository.class);
     public ObjectMapper objectMapper = new ObjectMapper();
     private final File file;
 
+    /** CONSTRUCTORS **/
     public DataRepository() {
         this.file = new File("./src/main/resources/data.json");
     }
@@ -20,20 +26,38 @@ public class DataRepository {
         this.file = new File(path);
     }
 
+    /** METHODS **/
+
+    /**
+     * Read a file and turn the data in a JsonNode in order to exploit them
+     *
+     * @return JsonNode of the Data in the file.
+     * @throws RuntimeException if something goes wrong with the reading of the file
+     */
     public JsonNode getData() {
         try {
+            logger.info("Data collected from file");
             return objectMapper.readTree(file);
 
         } catch (IOException e) {
+            logger.error("Something went wrong trying to collect the data from the file" + e);
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * Write the JsonNode with the new data into the JSON file
+     *
+     * @param data a JsonNode with the updated data
+     * @throws RuntimeException
+     */
     public void writeData(JsonNode data) {
         try {
+            logger.info("File updated with the new datas");
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(file, data);
-        } catch (IOException ex) {
-            throw new RuntimeException(ex);
+        } catch (IOException e) {
+            logger.error("Something went wrong with the writing of the file" + e);
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/com/safetynet/alerts/repository/FireStationRepository.java
+++ src/main/java/com/safetynet/alerts/repository/FireStationRepository.java
@@ -13,30 +13,45 @@ import java.util.List;
 
 @Repository
 public class FireStationRepository {
+    /** VARIABLES **/
     List<FireStation> fireStations;
     private final DataRepository dataRepository;
 
+    /** CONSTRUCTOR **/
     public FireStationRepository(DataRepository dataRepository) {
         this.dataRepository = dataRepository;
         createListFireStations();
     }
 
+    /**
+     * Take a JsonNode and fetch the values for the key "fireStations" in order to create a list of FireStations
+     *
+     * @throws RuntimeException if an error occur while creating the list
+     */
     public void createListFireStations() {
         try {
             JsonNode data = dataRepository.getData();
-            JsonNode fireStatioNode = data.get("firestations");
-            TypeReference<List<FireStation>> typeReferenceList = new TypeReference<List<FireStation>>() {};
-            List<FireStation> fireStations = new ObjectMapper().readValue(fireStatioNode.traverse(), typeReferenceList);
-            this.fireStations = fireStations;
+            JsonNode fireStationsNode = data.get("firestations");
+            TypeReference<List<FireStation>> typeReferenceList = new TypeReference<>() {};
+            this.fireStations = new ObjectMapper().readValue(fireStationsNode.traverse(), typeReferenceList);
         } catch (IOException e) {
             throw new RuntimeException("Error while creating FireStations List", e);
         }
     }
 
+    /**
+     * @return the list of fireStations
+     */
     public List<FireStation> findAll() {
         return fireStations;
     }
 
+    /**
+     * Browse through the fireStations to create a list of addresses covered by the given station number
+     *
+     * @param stationNumber a String representing the number of the station
+     * @return a List of String with the addresses covered by the station number
+     */
     public ArrayList<String> getCoveredAddresses(String stationNumber) {
         ArrayList<String> coveredAddresses = new ArrayList<>();
 
@@ -48,6 +63,12 @@ public class FireStationRepository {
         return coveredAddresses;
     }
 
+    /**
+     * Browse through the fireStations looking for the number of the station covering the given address
+     *
+     * @param address a String of an address
+     * @return a String representing the number of the fireStation covering this address
+     */
     public String getStationNumber(String address) {
         for(FireStation firesStation : findAll()) {
             if(firesStation.getAddress().equals(address)) {
@@ -57,13 +78,24 @@ public class FireStationRepository {
         return null;
     }
 
-    public FireStation save(FireStation fireStation) throws IOException {
+    /**
+     * Add a new fireStation to the list and update the file.
+     *
+     * @param fireStation a new FireStation to add
+     * @return the added fireStation
+     */
+    public FireStation save(FireStation fireStation) {
         fireStations.add(fireStation);
         updateFireStationsList(fireStations);
         return fireStation;
     }
 
-    public void delete(String inputAddress) throws IOException {
+    /**
+     * Delete the fireStation matching the given address
+     *
+     * @param inputAddress a String representing the address we want to delete
+     */
+    public void delete(String inputAddress) {
         for (FireStation firesStation : fireStations) {
             if(firesStation.getAddress().equals(inputAddress)) {
                 fireStations.remove(firesStation);
@@ -74,6 +106,12 @@ public class FireStationRepository {
         throw new IllegalArgumentException("FireStation not found: " + inputAddress);
     }
 
+    /**
+     * Update a given fireStation with the new data
+     *
+     * @param inputFireStation a FireStation with updated data
+     * @return the updated FireStation
+     */
     public FireStation update(FireStation inputFireStation) {
         for (FireStation firesStation : fireStations) {
             if(firesStation.getAddress().equals(inputFireStation.getAddress())) {
@@ -85,6 +123,11 @@ public class FireStationRepository {
         throw new IllegalArgumentException("FireStation not found: " + inputFireStation);
     }
 
+    /**
+     * Turn the FireStation list into a JsonNode in order to write in the JSON file as values for the key "firestations"
+     *
+     * @param fireStations the list of fireStations with new datas to write to the JSON File
+     */
     public void updateFireStationsList(List<FireStation> fireStations) {
         ObjectNode rootNode = (ObjectNode) dataRepository.getData();
         ObjectMapper objectMapper = new ObjectMapper();
