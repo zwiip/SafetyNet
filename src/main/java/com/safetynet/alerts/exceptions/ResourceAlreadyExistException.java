a9b2746e9857f369b01bc7048fa242f4eef4091f
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-09-18 11:37:33 +0200

    adding data validation to firestation creation

---
+++ src/main/java/com/safetynet/alerts/exceptions/ResourceAlreadyExistException.java
@@ -0,0 +1,11 @@
+package com.safetynet.alerts.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class ResourceAlreadyExistException extends RuntimeException {
+    public ResourceAlreadyExistException(String message) {
+        super(message);
+    }
+}
